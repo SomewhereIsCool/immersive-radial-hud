7c1f78e37e8f86cea6404708989ac87917e7d2eb
SomewhereIsCool <somewhereiscool@gmail.com>
2024-12-20 02:34:37 -0500

    Add official language to the keybind configurations

---
+++ src/main/java/net/somewhereiscool/immersivehud/hud/HUDLanguageProvider.java
@@ -0,0 +1,18 @@
+package net.somewhereiscool.immersivehud.hud;
+
+import net.somewhereiscool.immersivehud.ImmersiveRadialHUD;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.LanguageProvider;
+
+// @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
+public class HUDLanguageProvider extends LanguageProvider {
+    public HUDLanguageProvider(PackOutput output) {
+        super(output, ImmersiveRadialHUD.MODID, "en_us");
+    }
+
+    @Override
+    protected void addTranslations() {
+        this.add(HUDKeybind.getCategory(), "HUD Keybinds");
+    }
+
+}
