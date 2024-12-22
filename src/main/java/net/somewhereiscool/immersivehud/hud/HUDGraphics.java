3221246c8891551ad0bdc147d8b1541d88cc618d
SomewhereIsCool <somewhereiscool@gmail.com>
2024-12-22 17:30:50 -0500

    Select item when selected, setting up offhand implementation

--- src/main/java/net/somewhereiscool/immersivehud/hud/HUDGraphics.java
+++ src/main/java/net/somewhereiscool/immersivehud/hud/HUDGraphics.java
@@ -3,29 +3,30 @@ package net.somewhereiscool.immersivehud.hud;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.ImageWidget;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.neoforged.neoforge.client.event.ScreenEvent;
-
-import java.awt.event.KeyEvent;
-
-import static net.minecraft.world.item.Items.BIRCH_LOG;
+import org.lwjgl.glfw.GLFW;
 
 public class HUDGraphics extends Screen {
     private final KeyMapping keybind;
-    private static final ResourceLocation CROSSHAIR = ResourceLocation.fromNamespaceAndPath("minecraft", "textures/gui/sprites/hud/crosshair.png");
     private static final ResourceLocation SLOT = ResourceLocation.fromNamespaceAndPath("minecraft", "textures/gui/sprites/container/inventory/effect_background_small.png");
-    private static final Integer RADIUS = 50;
+    private static final Integer MAX_RADIUS = 50;
 
-    int xCenter;
-    int yCenter;
+    private int xCenter;
+    private int yCenter;
+
+    private double currentDegree;
+    private double degreeFactor;
+    private int degreeSelected;
+    private boolean shouldGoToOffhand = false;
+
+    ItemStack heldItem = Minecraft.getInstance().player.getMainHandItem();
+    ItemStack offhandItem = Minecraft.getInstance().player.getOffhandItem();
 
     protected HUDGraphics(Component title) {
         super(title);
@@ -38,6 +39,22 @@ public class HUDGraphics extends Screen {
     }
 
     @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if(button == GLFW.GLFW_MOUSE_BUTTON_RIGHT) {
+            shouldGoToOffhand = true;
+            onClose();
+            removed();
+        }
+
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    public boolean keyReleased(int keyCode, int scanCode, int modifiers) {
+        onClose();
+        return super.keyReleased(keyCode, scanCode, modifiers);
+    }
+
+    @Override
     protected void renderBlurredBackground() {
         return;
     }
@@ -46,10 +63,9 @@ public class HUDGraphics extends Screen {
     protected void init() {
         super.init();
 
+        // Add widgets and precomputed values
         xCenter = (this.width / 2) - 8;
         yCenter = (this.height / 2) - 8;
-
-        // Add widgets and precomputed values
     }
 
     @Override
@@ -61,6 +77,9 @@ public class HUDGraphics extends Screen {
     @Override
     public void onClose() {
         // Stop any handlers here
+        this.minecraft.player.getInventory().setSelectedHotbarSlot(degreeSelected);
+
+
 
         // Call last in case it interferes with the override
         super.onClose();
@@ -81,36 +100,42 @@ public class HUDGraphics extends Screen {
         // super.render(graphics, mouseX, mouseY, partialTick);
     }
 
-
-
     public void drawItems(GuiGraphics graphics, int mouseX, int mouseY) {
         // Get items and make a circle out of it
         // I would need to access the current players inventory
 
         int maxItems = Inventory.SELECTION_SIZE;
-        double degreeFactor = (double) 360 / maxItems;
-        double currentDegree = -90;
+        degreeFactor = (double) 360 / maxItems;
+        currentDegree = -90;
 
         for(int i = 0; i < maxItems; i++) {
-
-            int xOffset = (int) (RADIUS * Math.cos(Math.toRadians(currentDegree)));
-            int yOffset = (int) (RADIUS * Math.sin(Math.toRadians(currentDegree)));
+            int xOffset = (int) (MAX_RADIUS * Math.cos(Math.toRadians(currentDegree)));
+            int yOffset = (int) (MAX_RADIUS * Math.sin(Math.toRadians(currentDegree)));
 
             ItemStack item = this.minecraft.player.getInventory().getItem(i);
             graphics.renderItem(item, xCenter + xOffset, yCenter + yOffset);
 
             currentDegree += degreeFactor;
         }
+
+        calculateDesiredSlot(graphics, mouseX, mouseY);
     }
 
-
     public void calculateDesiredSlot(GuiGraphics graphics, int mouseX, int mouseY) {
-        // Calculates the angle by getting difference from cursor and center of the screen
-        int xDiff = xCenter-mouseX;
-        int yDiff = yCenter-mouseY;
+        // Calculates the difference from the cursor to the center of the screen
+        int xDiff = xCenter - mouseX; // Reversed xDiff to maintain screen coordinate consistency
+        int yDiff = yCenter - mouseY; // Reversed yDiff to maintain screen coordinate consistency
 
-        double angle = Math.tan((double) yDiff/xDiff);
+        double angle = Math.toDegrees(Math.atan2(yDiff, xDiff));
+        angle -= degreeFactor * 2;
 
+        // Normalize the angle to [0, 360]
+        if (angle < 0) {
+            angle += 360;
+        }
+
+        // Determine the selected slot
+        degreeSelected = (int) (angle / degreeFactor);
 
     }
 }
