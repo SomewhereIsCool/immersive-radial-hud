7bc885be4200a9f1d3cf1ca89064d55b575ec858
SomewhereIsCool <somewhereiscool@gmail.com>
2025-01-01 20:20:36 -0500

    Add empty_health_bar.png ResourceLocation

---
+++ src/main/java/net/somewhereiscool/immersivehud/hud/HUDTextures.java
@@ -0,0 +1,13 @@
+package net.somewhereiscool.immersivehud.hud;
+
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
+import net.somewhereiscool.immersivehud.ImmersiveRadialHUD;
+
+public class HUDTextures {
+    public static ResourceLocation EMPTY_HEALTH_BAR = ResourceLocation.
+            fromNamespaceAndPath(ImmersiveRadialHUD.MODID, "textures/gui/sprites/hud/empty_health_bar.png");
+
+
+
+}
