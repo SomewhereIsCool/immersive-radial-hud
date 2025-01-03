88e3d3d5324720bf4133a256f5a6a8bf7b029ded
SomewhereIsCool <somewhereiscool@gmail.com>
2025-01-02 21:50:27 -0500

    Added textures for health and hunger

--- src/main/java/net/somewhereiscool/immersivehud/hud/HUDTextures.java
+++ src/main/java/net/somewhereiscool/immersivehud/hud/HUDTextures.java
@@ -6,7 +6,10 @@ import net.somewhereiscool.immersivehud.ImmersiveRadialHUD;
 public class HUDTextures {
     public static ResourceLocation EMPTY_HEALTH_BAR = ResourceLocation.
             fromNamespaceAndPath(ImmersiveRadialHUD.MODID, "textures/gui/sprites/hud/empty_health_bar.png");
-
-
-
+    public static ResourceLocation FULL_HEALTH_BAR = ResourceLocation.
+            fromNamespaceAndPath(ImmersiveRadialHUD.MODID, "textures/gui/sprites/hud/full_health_bar.png");
+    public static ResourceLocation EMPTY_HUNGER_BAR = ResourceLocation.
+            fromNamespaceAndPath(ImmersiveRadialHUD.MODID, "textures/gui/sprites/hud/empty_hunger_bar.png");
+    public static ResourceLocation FULL_HUNGER_BAR = ResourceLocation.
+            fromNamespaceAndPath(ImmersiveRadialHUD.MODID, "textures/gui/sprites/hud/full_hunger_bar.png");
 }
