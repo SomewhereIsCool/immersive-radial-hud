909a010f2e769d0c46138ca6e399b5b98894670b
SomewhereIsCool <somewhereiscool@gmail.com>
2025-01-08 10:40:42 -0500

    Add files for Fabric and NeoForge

---
+++ fabric/src/client/java/net/somewhereiscool/minimalradialhud/hud/MinimalRadialHUDClient.java
@@ -0,0 +1,17 @@
+package net.somewhereiscool.minimalradialhud.hud;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Logger;
+
+import static net.somewhereiscool.minimalradialhud.hud.MinimalRadialHUD.MOD_ID;
+
+public class MinimalRadialHUDClient implements ClientModInitializer {
+    //public static final Logger LOGGER = (Logger) LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+        // LOGGER.info("YIPPEE!");
+    }
+}
---
+++ fabric/src/main/java/net/somewhereiscool/minimalradialhud/hud/MinimalRadialHUD.java
@@ -0,0 +1,12 @@
+package net.somewhereiscool.minimalradialhud.hud;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MinimalRadialHUD implements ModInitializer {
+    public static final String MOD_ID = "minimalradialhud";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
